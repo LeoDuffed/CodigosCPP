182b7f74a004edd789b0b49ed59c5f19e139499c
LeoDuffed <carlosmroder@gmail.com>
2024-12-30 22:44:36 -0600

    Iniciando el proyecto

--- Cine.cpp
+++ Cine.cpp
@@ -42,17 +42,42 @@ struct Pelicula{
     struct Duracion *duracion;
     struct Estreno *estreno;
     struct Genero genero;
-    struct Pelicula *ant, *sig;
+    struct Pelicula *ant, *sig; // Todas las peliculas estan enlazadas entre si.
 
 };
 
-struct Catalogo{
+struct Catalogo{ // Estructura mas grande
 
     int longitud;
     struct Pelicula *pelicula;
 };
 
+Catalogo *lista = NULL; // Inicializamos la lista en NULL (vacia)
+
+Pelicula *CrearNodo(string nombre, int precio, string genero1, string genero2, string genero3, int anio, int mes, int dia, int hora, int min){
+
+    Pelicula *aux;
+
+    return aux;
+}
+
+void AgregarPelicula (string nombre, int precio, string genero1, string genero2, string genero3, int anio, int mes, int dia, int hora, int min){
+
+    if (lista == NULL){
+        lista = new Catalogo; // Pedimos memoria para crear una estructura de tipo Catologo.
+        lista->longitud = 1;
+        lista->pelicula = CrearNodo(nombre, precio, genero1, genero2, genero3, anio, mes, dia, hora, min);
+    }
+
+}
+
 int main(){
+
+	AgregarPelicula("La era del hielo",10000,"aventura","comedia"," ",2022,11,4,1,30);
+	AgregarPelicula("titanic",7000,"romance","drama"," ",1997,12,19,3,15);
+	AgregarPelicula("Harry Potter y la piedra filosofal",6500,"fantasia","aventura"," ",2001,11,16,2,32);
+	AgregarPelicula("Avatar",5200,"animacion","aventura","drama",2009,12,18,2,42);
+	AgregarPelicula("El rey Leon",7000,"romance","drama","aventura",1994,6,15,1,28);
 
     return 0;
 }
