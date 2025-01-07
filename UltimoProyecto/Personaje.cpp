689d125e216bf5c4a2681acc5477ff2acd1d0f91
LeoDuffed <carlosmroder@gmail.com>
2025-01-07 00:12:38 -0600

    Ultimo proyecto

---
+++ UltimoProyecto/Personaje.cpp
@@ -0,0 +1,79 @@
+#include "Personaje.h"
+#include <iostream> 
+using namespace std;
+
+Personaje :: Personaje(){
+
+    nombre = "";
+    vida = 0;
+    nivel = 0;
+    defenzaFisica = 0;
+    defenzaMagica = 0;
+    velocidadMovimiento = 0;
+    velocidadATK = 0;
+    rango = 0;
+
+}
+
+Personaje :: Personaje (string nombre, int vida, int nivel, int defenzaFisica, int defenzaMagica, int velocidadMovimiento, int velocidadATK, int rango){
+
+    this -> nombre = nombre;
+    this -> vida = vida;
+    this -> nivel = nivel;
+    this -> defenzaFisica = defenzaFisica;
+    this -> defenzaMagica = defenzaMagica;
+    this -> velocidadMovimiento = velocidadMovimiento;
+    this -> velocidadATK = velocidadATK;
+    this -> rango = rango;
+
+}
+
+void Personaje :: setNombre(string nombre){
+    this -> nombre = nombre;
+}
+void Personaje :: setVida(int vida){
+    this -> vida = vida;
+}
+void Personaje :: setNivel(int nivel){
+    this -> nivel = nivel;
+}
+void Personaje :: setDefenzaFisica(int defenzaFisica){
+    this -> defenzaFisica = defenzaFisica;
+}
+void Personaje :: setDefenzaMagica(int defenzaMagica){
+    this -> defenzaMagica = defenzaMagica;
+}
+void Personaje :: setVelocidadMovimiento(int velocidadMovimiento){
+    this -> velocidadMovimiento = velocidadMovimiento;
+}
+void Personaje :: setVelocidadATK(int velocidadATK){
+    this -> velocidadATK = velocidadATK;
+}
+void Personaje :: setRango(int rango){
+    this -> rango = rango;
+}
+
+string Personaje :: getNombre(){
+    return nombre;
+}
+int Personaje :: getNivel(){
+    return nivel;
+}
+int Personaje :: getVida(){
+    return vida;
+}
+int Personaje :: getDefenzaFisica(){
+    return defenzaFisica;
+}
+int Personaje :: getDefenzaMagica(){
+    return defenzaMagica;
+}
+int Personaje :: getVelocidadMovimiento(){
+    return velocidadMovimiento;
+}
+int Personaje :: getVelocidadATK(){
+    return velocidadATK;
+}
+int Personaje :: getRango(){
+    return rango;
+}
---
+++ UltimoProyecto/Personaje.h
@@ -0,0 +1,43 @@
+#ifndef Personaje_H
+#define Personaje_H
+#include <iostream> 
+using namespace std;
+
+class Personaje{
+
+    private: 
+    string nombre;
+    int vida;
+    int nivel;
+    int defenzaFisica;
+    int defenzaMagica;
+    int velocidadMovimiento;
+    int velocidadATK;
+    int rango;
+    public:
+
+    Personaje();
+    ~Personaje(){}
+    Personaje(string nombre, int vida, int nivel, int defenzaFisica, int defenzaMagica, int velocidadMovimiento, int velocidadATK, int rango);
+    
+    void setNombre(string nombre);
+    void setVida(int vida);
+    void setNivel(int nivel);
+    void setDefenzaFisica(int defenzaFisica);
+    void setDefenzaMagica(int defenzaMagica);
+    void setVelocidadMovimiento(int velocidadMovimiento);
+    void setVelocidadATK(int velocidadATK);
+    void setRango(int rango);
+
+    string getNombre();
+    int getNivel();
+    int getVida();
+    int getDefenzaFisica();
+    int getDefenzaMagica();
+    int getVelocidadMovimiento();
+    int getVelocidadATK();
+    int getRango();
+
+};
+
+#endif
