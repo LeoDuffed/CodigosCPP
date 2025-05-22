cdf46194c5cf53a8bf2487cc756c451598489dd1
LeoDuffed <carlosmroder@gmail.com>
2025-05-21 22:31:34 -0600

    Nuevo mini proyecto

---
+++ Repaso2/Polinomio.h
@@ -0,0 +1,17 @@
+#ifndef POLINOMIO_H
+#define POLINOMIO_H
+#include <iostream> 
+using namespace std;
+
+class Polinomio{
+
+    private: 
+
+    public: 
+    Polinomio();
+    ~Polinomio(){}
+    Polinomio();
+
+};
+
+#endif
