567a24b4f78aad804c3b3e56cd0011e40c407d65
LeoDuffed <carlosmroder@gmail.com>
2025-06-10 15:49:40 -0600

    Cambios en las clases

--- ServicioStreaming/Episodio.h
+++ ServicioStreaming/Episodio.h
@@ -2,6 +2,13 @@
 #define EPISODIO_H
 #include <iostream>
 
+/* 
+    Ocupe un struct para usar lo en la clase Serie,
+    para poder hacer esta codigo mas reutilizable en caso de que
+    le quiera agregar otras opciones mas a mi clase Serie para cada 
+    capitulo. Ademas de que mi codigo se ve mas organizado, es mucho
+    mas facil manejar arreglos, para los capitulos de cada serie.
+*/
 struct Episodio{
     std :: string titulo;
     int temoporada;
--- ServicioStreaming/Serie.cpp
+++ ServicioStreaming/Serie.cpp
@@ -2,5 +2,42 @@
 
 Serie :: Serie(int id, std :: string nombre, int hora, int min, std :: string genero, int calificacion) :
 Video(id, nombre, hora, min, genero, calificacion), numCap(0), maxCap(10){
+    /* 
+        Pedimos memoria dinamica para crear un arreglo de 
+        maxCap de elementos de tipo Episodio.
+
+        capSeries guarda la direccion de memoria de inicio del bloque.
+    */
     capSeries = new Episodio[maxCap];
+}
+
+Serie :: ~Serie(){
+    // Como estamos usando new, destruimos con delete
+    delete[] capSeries; 
+}
+
+std :: string Serie :: getNombre() const{
+    return nombre;
+}
+
+void Serie :: agregarEpisodio(std :: string titulo, int temporada, int calificacion){
+    // Este metodo esta hecho para agregar los episodios de cada serie
+
+    if(numCap >= maxCap){
+        return;
+    }
+    capSeries[numCap++] = {titulo, temporada, calificacion};
+
+    float suma = 0;
+
+    for(int i = 0; i < numCap; i++){
+        suma += capSeries[i].calificacion;
+    }
+}
+void Serie :: mostrarVideos() const{
+    std :: cout << "\n\nSerie: " << nombre << std :: endl;
+    std :: cout << "Genero: " << genero << std :: endl;
+    for(int i = 0; i < numCap; i++){
+        std :: cout << "    -T" << capSeries[i].temoporada << ": " << capSeries[i].titulo << " (" << capSeries[i].calificacion << "/5)" << std :: endl;
+    }
 }
