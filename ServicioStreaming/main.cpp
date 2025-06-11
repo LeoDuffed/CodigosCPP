20eadc249d68d6bf7e5510200f0a52a029b7ba4a
LeoDuffed <carlosmroder@gmail.com>
2025-06-10 20:50:58 -0600

    no se puede usar la libreria vector

--- ServicioStreaming/main.cpp
+++ ServicioStreaming/main.cpp
@@ -1,102 +1,17 @@
 #include "StreamHub.h"
 #include <fstream>
 #include <sstream>
-#include <vector> // Uso vector para guardar los vides en un arreglo
 
 // Esta funcion que recibe por referencia un objeto de tipo StreamHub
 void cargarDatos(StreamHub& sistem){ 
-    // Codigo para cargar datos de Peliculas
-    std :: ifstream archivoPeliculas("db/movies.csv"); 
-    // Va a almacenar cada linea del archivo leido con 'getline'
-    std :: string allPeliculas;
+    std :: ifstream archivoMovies("db/movies.csv");
+    std :: string allMovies;
 
-    while(getline(archivoPeliculas, allPeliculas)){
-        // Creamos un stream de texto a partir de la linea leida del documetno
-        std :: stringstream ss(allPeliculas); 
-        // Almacenamos las partes separadas de la linea como el id, el nombre, y las otras
-        std :: vector<std :: string> espacios; 
-        std :: string espacio;
-
-        while(getline(ss, espacio, ',')){
-            espacios.push_back(espacio);
-        }
-
-        // Cargamos cada espacio del vector con la informacion de cada pelicula
-        if(espacios.size() > 5){
-            int id = stoi(espacios[0]);
-            std :: string nombre = espacios[1];
-            int hora = stoi(espacios[2]);
-            int min = stoi(espacios[3]);
-            std :: string genero = espacios[4];
-            int calificacion = stoi(espacios[5]);
-            
-            // Crear un nuevo objeto de tipo Movies con los valores obtenidos del archivo
-            Movies* newMovie = new Movies(id, nombre, hora, min, genero, calificacion);
-            sistem.addVideos(newMovie);
-        }
+    while(getline(archivoMovies, allMovies)){
+        std :: stringstream ss(allMovies);
+        std :: string idStr, nombre, horaStr, minStr, genero, calificacionStr;
     }
 
-    archivoPeliculas.close();
-
-    // Codigo para cargar datos de Series
-    std :: ifstream archivoSeries("db/Series.csv");
-    std :: string allSeries;
-
-    int MAX_SERIES = 50;
-    Serie* seriesCargadas[MAX_SERIES]; // Arreglo maximo de 50 espacios
-    int totalSeries = 0;
-
-    while(getline(archivoSeries, allSeries)){
-        std :: stringstream ss(allSeries);
-        std :: vector<std :: string> espacios;
-        std :: string espacio;
-
-        while(getline(ss, espacio, ',')){
-            espacios.push_back(espacio);
-        }
-
-        if(espacios.size() >= 9){
-            int id = stoi(espacios[0]);
-            std :: string nombre = espacios[1];
-            int hora = stoi(espacios[2]);
-            int min = stoi(espacios[3]);
-            std :: string genero = espacios[4];
-            int calificacion = stoi(espacios[5]);
-            std :: string tituloEpi = espacios[6];
-            int temporada = stoi(espacios[7]);
-            int califCap = stoi(espacios[8]);
-            
-            Serie* authSeries = nullptr;
-            /*
-                Itera sobre todas las series que se cargaron y si encuentra 
-                una serie cuyo nombre coincide con el que se esta leyendo en
-                la linea del archivo, se guarda un puntero a esa serie.
-            */
-            for(int i = 0; i < totalSeries; i++){
-                if(seriesCargadas[i] -> getNombre() == nombre){
-                    authSeries = seriesCargadas[i];
-                    break;
-                }
-            }
-            /*
-                Si no encontro una serie con ese nombre en la if pasado, 
-                creamos una nueva instancia de la clase Serie, se agrega
-                al arreglo de seriesCargadas, se incrementa el contado 
-                total de series "totalSeries" y se agrega con addVideos 
-                para que se pueda imprimir despues.
-            */
-            if(!authSeries){ // Si no encuentra una serie con el nombre 
-                authSeries = new Serie(id, nombre, hora, min, genero, calificacion);
-                seriesCargadas[totalSeries++] = authSeries;
-                sistem.addVideos(authSeries);
-            }
-
-            authSeries -> addEpisod(tituloEpi, temporada, califCap);
-        }
-    }
-    archivoSeries.close();
-
-    std :: cout << "\n\nDatos cargados con exito!!" << std :: endl;
 }
 
 // Main
