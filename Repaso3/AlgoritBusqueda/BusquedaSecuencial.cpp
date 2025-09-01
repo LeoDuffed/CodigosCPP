f1a5d7cd5f2dc0b4fcb4b4ee663af0410c988bda
LeoDuffed <carlosmroder@gmail.com>
2025-08-31 18:14:00 -0600

    Codigo nuevo de BusquedaSecuencial

--- Repaso3/AlgoritBusqueda/BusquedaSecuencial.cpp
+++ Repaso3/AlgoritBusqueda/BusquedaSecuencial.cpp
@@ -2,24 +2,31 @@
 #include <cstdlib>
 #include <ctime>
 
-void IniciarArreglo(int array[100]){ for(int i=0;i<100;i++) array[i]=rand()%100; }
-void MostrarArreglo(int array[100]){ for(int i=0;i<100;i++) std::cout<<array[i]<<" , ";}
-void BusquedaSecuencial(int num, int array[100]){
-    for(int i=0;i<100;i++){
-        if(array[i]==num){
-            std::cout<<"Numero encontrado en la posicion: "<<i<<"\n";
-            break;
-        } 
-    }
-    std::cout<<"No se encontro el numero: "<<num<<"\n";
+template <std::size_t N>
+void IniciarArreglo(int (&array)[N]) { for (std::size_t i = 0; i < N; ++i) array[i] = std::rand() % 100; }
+
+template <std::size_t N>
+void MostrarArreglo(const int (&array)[N]) { // array es una referencia (&) a un arreglo ([N]) de int
+    for (std::size_t i = 0; i < N; ++i) std::cout << array[i] << (i + 1 < N ? " , " : "\n");
 }
 
-int main(){
-    
-    int array[100], num=rand()%100;
+template <std::size_t N>
+int BusquedaSecuencial(const int (&array)[N], int num) {
+    for (std::size_t i = 0; i < N; ++i) {
+        if (array[i] == num) return static_cast<int>(i);
+    }
+    return -1;
+}
+
+int main() {
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+    int array[100];
     IniciarArreglo(array);
     MostrarArreglo(array);
-    std::cout<<"Numero buscado: "<<num<<"\n";
-    BusquedaSecuencial(num, array);
+    int num = std::rand() % 100;
+    std::cout << "Numero buscado: " << num << "\n";
+    int pos = BusquedaSecuencial(array, num);
+    if (pos >= 0) std::cout << "Numero encontrado en la posicion: " << pos << "\n";
+    else std::cout << "No se encontro el numero: " << num << "\n";
     return 0;
 }
