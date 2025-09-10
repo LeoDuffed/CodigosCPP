2e0c099c8b082d46ee2e7356a953ac0dc8bdf88b
LeoDuffed <carlosmroder@gmail.com>
2025-09-09 21:39:55 -0600

    ya quedo el algoritmo de quick sort

---
+++ Repaso3/AlgoritOrdenamiento/QuickSort.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+template<std::size_t N>
+void IniciarArreglo(int(&array)[N]){ for(std::size_t i=0;i<N;i++) array[i]=std::rand()%100; }
+
+template<std::size_t N>
+void MostrarArreglo(int(&array)[N]){ for(std::size_t i=0;i<N;i++) std::cout<<array[i]<<(i+1<N?" , ":"\n"); }
+
+/* Función de partición 
+Elige el último elemento como pivote.
+Reordena el arreglo de modo que todos los elementos <= pivote
+queden a su izquierda, y los > pivote a su derecha.
+Devuelve el índice final del pivote.
+*/ 
+template<std::size_t N>
+int particion(int (&array)[N], int l, int r){
+    int pivote = array[r]; // pivote = último elemento
+    int i = l - 1;         // i marca la frontera de los <= pivote
+
+    for(int j = l; j < r; j++){
+        if(array[j] <= pivote){
+            i++;
+            // Intercambiamos array[i] y array[j]
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+    // Colocamos el pivote en su lugar correcto
+    int temp = array[i+1];
+    array[i+1] = array[r];
+    array[r] = temp;
+
+    return i + 1; // posición final del pivote
+}
+
+/* QuickSort recursivo
+Ordena el rango [l, r] del arreglo (ambos incluidos).
+*/
+template<std::size_t N>
+void quickSortRec(int (&array)[N], int l, int r){
+    if(l < r){ 
+        // Particionamos el rango
+        int p = particion(array, l, r);
+        // Ordenamos la parte izquierda (menores al pivote)
+        quickSortRec(array, l, p - 1);
+        // Ordenamos la parte derecha (mayores al pivote)
+        quickSortRec(array, p + 1, r);
+    }
+}
+
+/* Wrapper QuickSort
+Ordena todo el arreglo llamando a quickSortRec en [0, N-1].
+*/
+template<std::size_t N>
+void QuickSort(int(&array)[N]){
+    quickSortRec(array, 0, N - 1);
+}
+
+
+int main(){
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+    int array[20];
+    IniciarArreglo(array);
+    std::cout << "Arreglo antes:\n";
+    MostrarArreglo(array);
+    QuickSort(array);
+    std::cout << "Arreglo despues:\n";
+    MostrarArreglo(array);
+    return 0;
+}
