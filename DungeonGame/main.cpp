6d00be625153f5300765504218c3b6080486ed83
LeoDuffed <carlosmroder@gmail.com>
2025-10-31 10:49:00 -0600

    Edite un poco el main

--- DungeonGame/main.cpp
+++ DungeonGame/main.cpp
@@ -2,33 +2,17 @@
 #include "Game.h"
 using namespace std;
 
-/**
- * main.cpp
- * Juego: Dungeon Crawler
- * ----------------------
- * - Carga el mapa (dungeon.txt) y los monstruos (monsters.txt)
- * - Si al inicio escribes la palabra secreta "mysticpath",
- *   se activa el modo CHEAT BFS, que muestra la ruta más corta.
- * - De lo contrario, se juega normalmente.
- */
-
 int main(){
-    cout << "=== Dungeon Crawler ===" << endl;
-    cout << "Introduce palabra secreta o presiona ENTER para jugar: ";
-
+    cout<<"Introduce palabra secreta o presiona ENTER para jugar: ";
     string clave;
     getline(cin, clave);
-    bool cheat = (clave == "mysticpath");
-
+    bool cheat = (clave == "cheatPath");
     Game game(64); // capacidad máxima de vértices = 64
-
     if(!game.init("data/dungeon.txt", "data/monsters.txt")){
         cout << "Error: no se pudo inicializar el juego." << endl;
         return 1;
     }
-
     game.run(cheat);
-
-    cout << "\nGracias por jugar Dungeon Crawler.\n";
+    cout << "\nGracias por jugar.\n";
     return 0;
 }
