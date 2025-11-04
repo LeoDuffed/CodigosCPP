5703eb602c8ef0cca831c7943578a8b6eb42e169
Eduardo <juantokx@gmail.com>
2025-11-04 11:35:21 -0600

    Definitions of SplayTree

---
+++ EstructurasDeDatos/SplayTree/SplayTree.h
@@ -0,0 +1,34 @@
+#ifndef SPLAYTREE_H
+#define SPLAYTREE_H
+
+#include "Node.h"
+
+template <typename T>
+class SplayTree {
+        Node<T>* root;
+        // Metodos utiles
+        bool isEmpty() const;   
+        int height(Node<T>* n) const;
+        int balanceFac(Node<T>* n)const;
+        int maxValue(int a, int b) const;
+        void print(Node<T>* node) const;
+        bool searchPath(Node<T>* root, const T& v) const;
+        // Rotaciones
+        Node<T>* rotateRight(Node<T>* node);
+        Node<T>* rotateLeft(Node<T>* node);
+        // Insertar y Eliminar
+        Node<T>* insert(Node<T>* node, const T& value);
+        Node<T>* deleteNode(Node<T>* node, const T& value, bool& removed);
+        Node<T>* smallestNode(Node<T>* node) const;
+        void clear(Node<T>* node); // limpiar arbol completo
+    public:
+        AVL(): root(nullptr) {}
+        ~AVL();
+        void clear();
+        bool deleteNode(const T& v);
+        void print();
+        void insert(const T& value);
+        bool searchPath(const T& node) const;
+};
+
+#endif
