8b8f2e37275abfecbbf58dc65e6246bd805ccdc0
LeoDuffed <carlosmroder@gmail.com>
2025-11-11 13:16:17 -0600

    Limpie mas codigo

--- EstructurasDeDatos/SplayTree/SplayTree.h
+++ EstructurasDeDatos/SplayTree/SplayTree.h
@@ -9,21 +9,11 @@
 template <typename T>
 class SplayTree {
         Node<T>* root;
-        // Metodos utiles
         bool isEmpty() const;   
-        int height(Node<T>* n) const;
-        int maxValue(int a, int b) const;
         void print(Node<T>* node) const;
-        bool searchPath(Node<T>* root, const T& v) const;
-        // Rotaciones
         Node<T>* rotateRight(Node<T>* node);
         Node<T>* rotateLeft(Node<T>* node);
-        // Insertar y Eliminar
-        Node<T>* smallestNode(Node<T>* node) const;
         void clear(Node<T>* node); 
-        Node<T>* rotateLeftRight(Node<T>* node);
-        Node<T>* rotateRightLeft(Node<T>* node);
-        Node<T>* search(Node<T>* root, const T& v);
         Node<T>* splay(Node<T>* root, const T& v);
 
     public:
@@ -33,7 +23,6 @@ class SplayTree {
         bool deleteNode(const T& v);
         void print();
         void insert(const T& value);
-        bool searchPath(const T& node) const;
         bool search(const T& v);
 
 
